088dbf3f5fd23063a5aa96cba284a8608d1c54f4
Darius <juchevicd@gmail.com>
2025-01-08 14:03:26 +0400

    add arrayList

--- String_praktiniai/S_06.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ String_praktiniai/S_06.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,25 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int oldest = 0;
+        while (true) {
 
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] words = input.split(",");
+
+            int age = Integer.parseInt(words[words.length - 1]);
+
+            System.out.println(age);
+            if (oldest < age){
+                oldest = +age;
+//                System.out.println(oldest);
+            }
+            System.out.println(oldest);
+        }
     }
 }
--- String_praktiniai/S_07.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ String_praktiniai/S_07.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,31 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int oldest = 0;
+        String newName = "";
+        String oldName = "";
+        while (true) {
 
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] words = input.split(",");
+
+            int age = Integer.parseInt(words[words.length - 1]);
+
+            newName = words[0];
+            System.out.println(age);
+            if (oldest < age){
+                oldest = age;
+                oldName = newName;
+                System.out.println("name " + newName);
+//                System.out.println(oldest);
+            }
+            System.out.println("Name of the oldest " + oldName);
+        }
     }
+
 }
