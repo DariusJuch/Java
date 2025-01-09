8c51059c55de79468e07d0b42e5723cbb6a7e748
Darius <juchevicd@gmail.com>
2025-01-09 09:23:03 +0400

    end string

--- String_praktiniai/S_08.PersonalDetails/src/main/java/PersonalDetails.java
+++ String_praktiniai/S_08.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,6 +6,29 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String longestName = "";
+        int years = 0;
+        int count = 0;
+        double avgYears = 0;
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            }
+            String[] parts = input.split(",");
+            if (longestName.length() < parts[0].length()) {
+                longestName = parts[0];
+            }
+            years += Integer.parseInt(parts[1]);
+            count++;
+            avgYears = (double) years / count;
+
+
+        }
+        System.out.println(longestName);
+        System.out.println(avgYears);
 
 
     }
